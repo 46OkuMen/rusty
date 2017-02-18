52d1d130eea8ab38d34fcd5f4db5ea4833468587
Max Silbiger <max.silbiger@gmail.com>
2017-02-17 22:43:52 -0500

    Fix lowercase text ROT25; add delays for comma/period

--- reinsert.py
+++ reinsert.py
@@ -21,31 +21,36 @@ for filename in files_to_reinsert:
     diff = 0
     if len(pointers) == 0: # VISUAL.COM
         diff = 0
-        GF.filestring = GF.filestring[:0x3ac7] + '\x1a' + GF.filestring[0x3ac8:] # font table subtraction hack
-        GF.filestring = GF.filestring[:0x403c] + '\x02' + GF.filestring[0x403d:] # halfwidth cursor hack
-        #removal_length = 0x3010-0x1462
-        #GF.filestring = GF.filestring[:0x1462] + '\x00'*removal_length + GF.filestring[0x3010:] # removing all non visual 1 data??
-        #assert len(GF.filestring) == len(GF.original_filestring)
-        #for t in [t for t in translations if t.location <= 0x13cd]:
-        #    for cc in CONTROL_CODES:
-        #        t.english = t.english.replace(cc, CONTROL_CODES[cc])
-        #        t.japanese = t.japanese.replace(cc, CONTROL_CODES[cc])
+        GF.filestring = GF.filestring[:0x3ac7] + '\x1a' + GF.filestring[0x3ac8:] # font table column subtrarction change (sub eax, 2000 -> sub eax, 1a00)
+        GF.filestring = GF.filestring[:0x403c] + '\x02' + GF.filestring[0x403d:] # halfwidth cursor incrementing         (add dx, +04   -> add dx, +02)
+        GF.filestring = GF.filestring[:0x4006] + '\xb4\x82' + GF.filestring[0x4008:] # SJIS fake prefix adding hack      (or ah, ah     -> mov ah, 82)
+        #GF.filestring = GF.filestring[:0x3e3a] + '\x01' + GF.filestring[0x3e3b:] # remove row subtraction by 0x1a        (sub al, 1a    -> sub al, 1)
+        GF.filestring = GF.filestring[:0x3e39] + '\x04\xdf\x90\x90\x90\x90\x90\x90' + GF.filestring[0x3e41:] # lowercase/uppercase fix
+        removal_length = 0x3010-0x1462
+        GF.filestring = GF.filestring[:0x1462] + '\x00'*removal_length + GF.filestring[0x3010:] # removing all non visual 1 data??
+        GF.filestring = GF.filestring[:0x403e] + '\x2c\x82' + GF.filestring[0x4040:] # comma pause handling
+        GF.filestring = GF.filestring[:0x4048] + '\x2e\x82' + GF.filestring[0x404a:] # period pause handling
+        assert len(GF.filestring) == len(GF.original_filestring), hex(len(GF.filestring)) + " " + hex(len(GF.original_filestring))
+        for t in [t for t in translations if t.location <= 0x13cd]:
+            for cc in CONTROL_CODES:
+                t.english = t.english.replace(cc, CONTROL_CODES[cc])
+                t.japanese = t.japanese.replace(cc, CONTROL_CODES[cc])
 
-        #    t.english = ascii_to_fullwidth_sjis(t.english)
+            #t.english = ascii_to_fullwidth_sjis(t.english)
             #print repr(t.english)
 
-        #    j = GF.filestring.index(t.japanese)
-        #    this_diff = len(t.english) - len(t.japanese)
-        #    if this_diff <= 0:
-        #        print t.english
-        #    diff += this_diff
-        #    GF.filestring = GF.filestring.replace(t.japanese, t.english, 1)
+            j = GF.filestring.index(t.japanese)
+            this_diff = len(t.english) - len(t.japanese)
+            if this_diff <= 0:
+                print t.english
+            diff += this_diff
+            GF.filestring = GF.filestring.replace(t.japanese, t.english, 1)
 
-        #GF.filestring = GF.filestring[:0x3010-diff] + GF.filestring[0x3010:] # get rid of some 00s
+        GF.filestring = GF.filestring[:0x3010-diff] + GF.filestring[0x3010:] # get rid of some 00s
 
-        #scene1_data = GF.filestring[0x632:0x1bb3]
-        ##scene1_new_location = len(GF.filestring) # 463e
-        ##print hex(scene1_new_location)
+        scene1_data = GF.filestring[0x632:0x1bb3]
+        #scene1_new_location = len(GF.filestring) # 463e
+        #print hex(scene1_new_location)
         #GF.filestring = GF.filestring[:0x77] + '\x3e\x46' + GF.filestring[0x79:] # edit scene 1 pointer
         #assert len(GF.filestring) == len(GF.original_filestring), hex(len(GF.filestring)) + " " + hex(len(GF.original_filestring))
         #GF.filestring += scene1_data
