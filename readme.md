4bf660fc8f1797fa4bee8998dd88750399efd35d
Max Silbiger <max.silbiger@gmail.com>
2017-03-01 04:38:03 -0500

    ASM for gameover/ending scroll; reinsert through ending scroll

--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
 # rusty
 ![Title screen, still not translated](http://46okumen.com/wp-content/uploads/2016/11/rusty_02.png)
 
-Romhacking utilities and notes developed as part of [46 OkuMen](http://46okumen.com/)'s English translation patch for *Rusty* (PC-98), a pretty great clone of Castlevania.
+Romhacking utilities and notes developed as part of [46 OkuMen](http://46okumen.com/)'s English translation patch for *Rusty* (PC-98), a pretty decent clone of Castlevania.
 
 ### Progress
 | Segment                 | %    | Strings     | 
 | ------------------------|-----:|:-----------:|
 | Opening Text Images     |  0%  |  (0 / 10)   |
 | Main Menu/System        | 38%  | (11 / 29)   |
-| CG Dialogue             | 67%  | (72 / 107)  |
+| CG Dialogue             | 92%  | (98 / 107)  |
 | Girl Rescue Dialogue    |100%  |(134 / 134)  |
 | Boss Boasting Dialogue  |100%  | (86 / 86)   |
 | Level Transition Images |100%  |  (3 / 3)    |
-| Total                   | 83%  |(306 / 369)  |
+| Total                   | 90%  |(332 / 369)  |
 
 
 ### How do I use this?
--- reinsert.py
+++ reinsert.py
@@ -1,5 +1,5 @@
 import os
-from rominfo import SRC_DISK_PATH, SRC_DISK_DIR, DEST_DISK_PATH, DEST_DISK_DIR, CONTROL_CODES
+from rominfo import SRC_DISK_PATH, SRC_DISK_DIR, DEST_DISK_PATH, DEST_DISK_DIR, CONTROL_CODES, SCENE_POINTERS
 from romtools.disk import Disk, Gamefile
 from romtools.dump import PointerExcel, DumpExcel, Translation
 
@@ -8,10 +8,10 @@ DestDisk = Disk(DEST_DISK_PATH)
 DumpXl = DumpExcel('rusty_dump.xlsx')
 PtrXl = PointerExcel('rusty_pointer_dump.xlsx')
 
-files_to_reinsert = ['VISUAL.COM', 'STORY1.COM', 'STORY2.COM', 'STORY3.COM', 'STORY4.COM', 'STORY5.COM', 'STORY6.COM',
-                     'STORY7.COM', 'STORY8.COM', 'STORY9.COM', 'STORY10.COM', 'ENEMY1.COM', 'ENEMY4.COM',
-                     'ENEMY9.COM', 'ENEMY10.COM' ]
-#files_to_reinsert = ['VISUAL.COM']
+#files_to_reinsert = ['VISUAL.COM', 'STORY1.COM', 'STORY2.COM', 'STORY3.COM', 'STORY4.COM', 'STORY5.COM', 'STORY6.COM',
+#                     'STORY7.COM', 'STORY8.COM', 'STORY9.COM', 'STORY10.COM', 'ENEMY1.COM', 'ENEMY4.COM',
+#                     'ENEMY9.COM', 'ENEMY10.COM' ]
+files_to_reinsert = ['VISUAL.COM']
 
 for filename in files_to_reinsert:
     scene_texts = []
@@ -23,18 +23,14 @@ for filename in files_to_reinsert:
     indexed_pointers = pointers.values()
     diff = 0
     if filename == 'VISUAL.COM':
-        #GF.edit(0x3ac7, '\x1a')     # font table column subtraction
-        #GF.edit(0x403c, '\x02')     # halfwidth cursor incrementing
-        #GF.edit(0x4006, '\xb4\x82') # prepend 0x82 to characters
-        #GF.edit(0x403e, '\x2c\x82') # comma pause handling
-        #GF.edit(0x4048, '\x2e\x82') # period pause handling
-        #GF.edit(0x3e39, '\x04\xdf\x90\x90\x90\x90\x90\x90') # fix lowercase char shifting by 1
+        GF.edit(0x3ac7, '\x1a')     # font table column subtraction
+        GF.edit(0x403c, '\x02')     # halfwidth cursor incrementing
+        GF.edit(0x4006, '\xb4\x82') # prepend 0x82 to characters
+        GF.edit(0x403e, '\x2c\x82') # comma pause handling
+        GF.edit(0x4048, '\x2e\x82') # period pause handling
+        GF.edit(0x3e39, '\x04\xdf\x90\x90\x90\x90\x90\x90') # fix lowercase char shifting by 1
 
         for i, p in enumerate(pointers.itervalues()):
-            #if any([pt.text_location >= 0x21ff for pt in p]): # first 5 scenes
-            #if any([pt.text_location >= 0x28b5 for pt in p]): # first 6 scenes
-            #    continue
-
             print "considering translations from pointer", p
 
             start = p[0].text_location
@@ -60,9 +56,40 @@ for filename in files_to_reinsert:
                 GF.filestring = GF.filestring.replace(t.japanese, t.english, 1)
 
             if next_p:
-                #if next_p[0].text_location >= 0x21ff:
-                #    print "it's 21ff, so skipping this"
-                #    continue
+                if next_p[0].text_location == pointers[SCENE_POINTERS[2]][0].text_location:
+                    stable_location = GF.filestring.index('\x07\x02\x76\x73\x33')
+                    print hex(stable_location)
+                    print "Scene 3 is next, so padding it here"
+                    # (diff is negative here)
+                    GF.filestring = GF.filestring[:stable_location] + '\x00'*((-1)*diff) + GF.filestring[stable_location:]
+                    diff = 0
+
+                #elif next_p[0].text_location == 0x1a0d:
+                #    stable_location = GF.filestring.index('\x10\x00\x5f\x76\x69\x73\x75\x61\x6c\x33\x2e')
+                #    print "Scene 4 is next, so padding it at", hex(stable_location)
+                #    GF.filestring = GF.filestring[:stable_location] + '\x00'*((-1)*diff) + GF.filestring[stable_location:]
+                #    diff = 0
+
+                #elif next_p[0].text_location == 0x1e30:
+                #    stable_location = GF.filestring.index('\x10\x00\x5f\x76\x69\x73\x75\x61\x6c\x35\x2e')
+                #    print "Scene 5 is next, so padding it at", hex(stable_location)
+                #    GF.filestring = GF.filestring[:stable_location] + '\x00'*((-1)*diff) + GF.filestring[stable_location:]
+                #    diff = 0
+
+
+                #elif next_p[0].text_location == 0x2230:
+                #    stable_location = GF.filestring.index('\x10\x00\x5f\x76\x69\x73\x75\x61\x6c\x34\x2e')
+                #    print "Scene 6 is next, so padding it at", hex(stable_location)
+                #    GF.filestring = GF.filestring[:stable_location] + '\x00'*((-1)*diff) + GF.filestring[stable_location:]
+                #    diff = 0
+
+                #elif next_p[0].text_location == 0x2892:
+                #    stable_location = GF.filestring.index('\x02\x0a\x00\x05\x01\x0a\x00\x07')
+                #    print "Ending is next, so padding it at", hex(stable_location)
+                #    GF.filestring = GF.filestring[:stable_location] + '\x00'*((-1)*diff) + GF.filestring[stable_location:]
+                #    diff = 0
+
+
                 for loc in next_p:
                     if loc.location > loc.text_location:
                         # Update the pointer location with the new diff.
@@ -79,65 +106,17 @@ for filename in files_to_reinsert:
                     print "editing %s with diff %s" % (loc, diff)
                     loc.edit(diff)
 
-        # So uh, how about that 151e71 control code?? Does it need moving?
-            # Try translating it without Gateau's first line. That'll keep the one line in place.
-            # If it's a pointer to 151e...
+        print "final diff is", diff
+        code_block_location = GF.filestring.index('\xe8\x99\x01\xb2\x0a\xb8\x13\x00')
+        if diff > 0:
+            GF.filestring = GF.filestring[:code_block_location-diff] + GF.filestring[code_block_location:] # blank part of the file
+        else:
+            GF.filestring = GF.filestring[:code_block_location] + '\x0d'*((-1)*diff) + GF.filestring[code_block_location:]
 
-        # 1420: fine, since it's before 142a and 1439
-
-        # 1430: oddly quick blinking animation?
-        # 1430:
-        # 1432: same as 1433
-        # 1433: same as 1434, maybe longer second frame
-        # 1434: no crash, but incomplete blink animation at ends of lines (open, slightly closed for a while, then back to open)
-        # 1435: no crash, but she gets derpy half closed eyes instead of the final blinking in each line
-
-        # 143b: "you monsters kidnapped!" crash (the 142a ani is blanked)
-        # 1450: "you monsters kidnapped!" crash, with weird stuttering blinking animations all throughout
-        # 1458: "you monsters kidnapped!" crash, derpy eyes the whole time, broken animation
-        # 1462: "save the gir" crash
-        #blank_until = 0x1458
-        #blank_length = blank_until-0x632
-        #GF.filestring = GF.filestring[:0x632] + '\x00'*blank_length + GF.filestring[blank_until:] # blank scene 1
-
-        GF.filestring = GF.filestring[:0x2ff4-diff] + GF.filestring[0x2ff4:] # blank part of the file
-
-        # So, that scene 2 crash. Doesn't have to do with the translations, control codes, or the ASM hacks.
-        # But it does have to do with large blank spaces in the ROM...?
-        # Can't just replace them with 20's, either. Weird.
-        # Solved by overwriting parts of scenes we don't care about, without blanking them at all.
-
-        # This is a problem with the control code 01 39 14.
-        # Some of the similar control codes in the previous section are like 01 20 06, 01 2d 06, etc.
-        # Probably pointing to something toward the beginning of each scene. (0x620, 0x62d, 0x1439, 0x142a, etc)
-        # 0x142a = 04 00 05 0a 00 02 01 03 00 05 04 46 ff 2e 14
-        # 0x1439 = 04 00 05 0a 02 05 03 0f 02 06 05 0f 02 04 03 0a 02 02 00 01 02 02 01 01 05 0a 00 02 02 02 04 01 03 01a 02 03 05 0f ff 3d 14
-
-        # Hey, that 14 2e - does it suggest some kind of loop to earlier in that list of bytes?
-            # If it did, it points to the the location 4 bytes after the beginning! (first 4 bytes = header? x/y coords?)
-
-
-        # vs2_00.mag = background
-        # vs2_01.mag = Gateau
-        # vs2_02.mag = Rusty
-        # Yep, they're pointers to stuff in the scene headers. But, before the stuff that gets pointed to!
-
-        # So they're basically pointers mixed in with text.
-        # Pointable stuff between ?(0x142a, 0x2fd1)
-        # So, look for stuff between (012a14) and (01d12f).
-
-        # Why is it reading two bytes at once for ir???
-
-        # Getting stuck at 8312:4419. "movsb, loop 4419"
-        # Probably pops something stupidly big off the stack, then loops forever
-
-        # Check the things that get pointed to in the headers. Are they code, or pointers to other things?
-
-        # Is it worth just trying to get the length of the blocks down??
-        # Scene 1 is ~2500 characters, and needs to be 193 shorter
-        # Can I get rid of any control codes with no repercussions?
-
-        # 05 28 is probably a "repeat last animation" control code.
+        SCENE_TO_TEST = 0     # 0-5 for scenes 1-6.
+        scene_diff = pointers[SCENE_POINTERS[SCENE_TO_TEST]][0].new_text_location - pointers[SCENE_POINTERS[0]][0].new_text_location
+        pointers[SCENE_POINTERS[0]][0].edit(scene_diff)
+        print pointers[SCENE_POINTERS[0]]
 
         assert len(GF.filestring) == len(GF.original_filestring), hex(len(GF.filestring)) + " " + hex(len(GF.original_filestring))
 
@@ -165,3 +144,5 @@ for filename in files_to_reinsert:
 
 
     GF.write(path_in_disk='\\RUSTY\\', compression=True)
+
+# TODO: Try padding each VISUAL.COM scene. Maybe no pointers will need edits!
